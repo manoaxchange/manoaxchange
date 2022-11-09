8de29d0c8788bf87f2bd6794a20d7cb57c5117a9
Giorgio <tran.giorgio@gmail.com>
2022-11-09 12:53:49 -1000

    Add Items schema, and update settings config file

---
+++ app/imports/api/items/Items.js
@@ -0,0 +1,31 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The ItemsCollection. It encapsulates state and variable values for items.
+ */
+class ItemsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ItemsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      image: String,
+      price: Number,
+      description: String,
+      name: String,
+      owner: String,
+      rating: Number,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Items = new ItemsCollection();
--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Items } from '../../api/items/Items';
 
 /* eslint-disable no-console */
 
@@ -9,10 +10,22 @@ const addData = (data) => {
   Stuffs.collection.insert(data);
 };
 
+const addDefaultItems = (data) => {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+};
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.forEach(data => addData(data));
+  }
+}
+
+// Initialize the ItemsCollection if empty.
+if (Items.collection.find().count() === 0) {
+  if (Meteor.settings.defaultItems) {
+    console.log('Creating default items.');
+    Meteor.settings.defaultItems.forEach(item => addDefaultItems(item))
   }
 }
