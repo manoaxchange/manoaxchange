df20a53507be16cf977e1aa2a2ad524dc5a800e8
Giorgio <tran.giorgio@gmail.com>
2022-11-13 19:44:24 -1000

    Add object-fit to have images fill card

--- app/imports/ui/pages/Shop.jsx
+++ app/imports/ui/pages/Shop.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Image } from 'react-bootstrap';
 import SidebarFull from '../components/sidebar/SidebarFull';
 
 const Item = () => (
   <Col xs={12} lg={4} xl={3} className="d-flex my-3">
-    <Card className="w-100">
-      <Card.Body>
-        test
+    <Card className="w-100" style={{ minHeight: '300px' }}>
+      <Card.Body className="d-flex align-items-center">
+        <Image width="100%" height="100%" src="https://imageio.forbes.com/specials-images/imageserve/5e086a2f25ab5d0007cf74ec/Oahu/0x0.jpg?format=jpg&crop=1866,1244,x1,y0,safe&width=960" style={{ objectFit: 'contain' }} />
       </Card.Body>
       <Card.Footer>
         test
@@ -28,7 +28,7 @@ const Shop = () => (
     {/* temporarily set the background to dark to test padding */}
     <SidebarFull />
     <Container fluid>
-      <Row>
+      <Row className="h-100">
         {testingCols().map(col => col)}
       </Row>
     </Container>
