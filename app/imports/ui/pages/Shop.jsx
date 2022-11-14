05bbe70cc1273fc2def9bc2d796512fc8ed21161
Giorgio <tran.giorgio@gmail.com>
2022-11-13 16:33:47 -1000

    Add shop route and shop page

---
+++ app/imports/ui/pages/Shop.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Shop = () => (
+  <div className="d-flex">
+    <div>
+      hello
+    </div>
+    <Container>
+      hello 2
+    </Container>
+  </div>
+);
+
+export default Shop;
