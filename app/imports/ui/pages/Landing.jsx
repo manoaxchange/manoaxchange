b1d3c9840c7f2f28d5771c4a7db580c5d575ddba
Giorgio <tran.giorgio@gmail.com>
2022-11-14 08:30:17 -1000

    Bold some phrases in Welcome section

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -16,10 +16,11 @@ const Landing = () => (
         <Row className="d-flex align-items-center justify-content-center">
           <Col xs={11} xl={9} className="d-flex align-items-center justify-content-center">
             <p className="w-100 py-3 px-3" style={{ wordSpacing: '2px', lineHeight: 1.8, letterSpacing: '0.4px' }}>
-              Large masses of students quickly come and go in the dorms of UH Manoa. This leads to lots of
-              campus specific items that are discarded by previous students. Our goal at ManoaXchange is to
-              promote and incentivize buying and selling goods between the students of UH Manoa. This
-              allows students to reuse and repurpose items that were used by other students, thereby reducing
+              Large masses of <b>students quickly come and go</b> in the dorms of UH Manoa. This leads to lots of
+              campus specific items that are discarded by previous students.
+              <b>Our goal at ManoaXchange is to
+                promote and incentivize buying and selling goods between the students of UH Manoa
+              </b>. This allows students to reuse and repurpose items that were used by other students, thereby reducing
               the amount of items that were going to be discarded.
             </p>
           </Col>
