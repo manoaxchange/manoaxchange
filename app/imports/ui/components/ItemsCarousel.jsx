27b4e74e2d7e88b656b3bd85e95bacccdb7a2979
Giorgio <tran.giorgio@gmail.com>
2022-11-13 23:14:45 -1000

    Remove shop now from carousel

--- app/imports/ui/components/ItemsCarousel.jsx
+++ app/imports/ui/components/ItemsCarousel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Carousel } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 
 const ItemsCarousel = () => (
   <Carousel fade>
@@ -8,20 +8,8 @@ const ItemsCarousel = () => (
         className="d-block w-100"
         src="images/macbook.jpg"
         alt="First slide"
-        style={{ height: '600px', objectFit: 'cover' }}
+        style={{ height: '660px', objectFit: 'cover' }}
       />
-      <Carousel.Caption>
-        <Button
-          className="mb-2"
-          variant="light"
-          href="/shop"
-          style={{ width: '150px', height: '50px' }}
-        >
-          <div className="w-100 h-100 d-flex align-items-center justify-content-center">
-            Shop Now
-          </div>
-        </Button>
-      </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item interval={2000}>
       <div className="bg-dark class">
@@ -29,41 +17,17 @@ const ItemsCarousel = () => (
           className="d-block w-100"
           src="images/clrs.jpeg"
           alt="Second slide"
-          style={{ height: '600px', objectFit: 'cover'}}
+          style={{ height: '660px', objectFit: 'cover'}}
         />
       </div>
-      <Carousel.Caption>
-        <Button
-          className="mb-2"
-          variant="light"
-          href="/shop"
-          style={{ width: '150px', height: '50px' }}
-        >
-          <div className="w-100 h-100 d-flex align-items-center justify-content-center">
-            Shop Now
-          </div>
-        </Button>
-      </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item interval={2000}>
       <img
         className="d-block w-100"
         src="images/moped.jpeg"
         alt="Third slide"
-        style={{ height: '600px', objectFit: 'cover'}}
+        style={{ height: '660px', objectFit: 'cover'}}
       />
-      <Carousel.Caption>
-        <Button
-          className="mb-2"
-          variant="light"
-          href="/shop"
-          style={{ width: '150px', height: '50px' }}
-        >
-          <div className="w-100 h-100 d-flex align-items-center justify-content-center">
-            Shop Now
-          </div>
-        </Button>
-      </Carousel.Caption>
     </Carousel.Item>
   </Carousel>
 );
