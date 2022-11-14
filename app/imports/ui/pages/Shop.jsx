7febea66f068f8625beb1b165c0721d36c8261d7
Giorgio <tran.giorgio@gmail.com>
2022-11-13 18:28:43 -1000

    Implement basic item alignment w/grid

--- app/imports/ui/pages/Shop.jsx
+++ app/imports/ui/pages/Shop.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import SidebarFull from '../components/sidebar/SidebarFull';
 
+const Item = () => (
+  <Col xs={12} lg={4} className="d-flex my-3">
+    <Card className="w-100">
+      <Card.Body>
+        test
+      </Card.Body>
+      <Card.Footer>
+        test
+      </Card.Footer>
+    </Card>
+  </Col>
+);
+
+const testingCols = () => {
+  const arr = [];
+  for (let i = 0; i < 9; i++) {
+    arr.push(<Item />);
+  }
+  return arr;
+};
+
 const Shop = () => (
-  <div className="d-flex">
+  <div className="d-flex overflow-auto">
     {/* temporarily set the background to dark to test padding */}
     <SidebarFull />
-    <Container>
-      hello 2
+    <Container fluid>
+      <Row className="overflow-auto">
+        {testingCols().map(col => col)}
+      </Row>
     </Container>
   </div>
 );
