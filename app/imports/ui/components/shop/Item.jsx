9aa8722635fb25e078d71cb5e8b3f55bb8abe0a5
Giorgio <tran.giorgio@gmail.com>
2022-11-15 13:33:11 -1000

    Add PropTypes to item, and removed temp function

--- app/imports/ui/components/shop/Item.jsx
+++ app/imports/ui/components/shop/Item.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Card, Col, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Item = () => (
+const Item = ({ item }) => (
   <Col xs={12} lg={4} xl={3} className="d-flex my-3">
     <Card className="w-100" style={{ minHeight: '300px' }}>
       <Card.Body className="d-flex align-items-center">
-        <Link to="/#">
-          <Image width="100%" height="100%" src="https://imageio.forbes.com/specials-images/imageserve/5e086a2f25ab5d0007cf74ec/Oahu/0x0.jpg?format=jpg&crop=1866,1244,x1,y0,safe&width=960" style={{ objectFit: 'contain' }} />
+        <Link to={`/item/${item._id}`}>
+          <Image width="100%" height="100%" src={item.image} style={{ objectFit: 'contain' }} />
         </Link>
       </Card.Body>
       <Card.Footer className="d-flex justify-content-between gap-2">
-        <a className="text-decoration-none text-dark" href="/#"><b>Item Name</b></a>
-        <b>$32.00</b>
+        <a className="text-decoration-none text-dark" href="/#"><b>{item.name}</b></a>
+        <b>${item.price}</b>
       </Card.Footer>
     </Card>
   </Col>
 );
+
+// Require a document to be passed to this component.
+Item.propTypes = {
+  item: PropTypes.shape({
+    name: PropTypes.string,
+    price: PropTypes.number,
+    image: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
 
 export default Item;
--- app/imports/ui/pages/Shop.jsx
+++ app/imports/ui/pages/Shop.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import SidebarFull from '../components/sidebar/SidebarFull';
 import Item from '../components/shop/Item';
 
-const testingCols = () => {
-  const arr = [];
-  for (let i = 0; i < 30; i++) {
-    arr.push(<Item />);
-  }
-  return arr;
-};
 
 const Shop = () => (
   <div className="d-flex">
@@ -18,7 +10,7 @@ const Shop = () => (
     <SidebarFull />
     <Container fluid className="min-vh-100">
       <Row>
-        {testingCols().map(col => col)}
+
       </Row>
     </Container>
   </div>
