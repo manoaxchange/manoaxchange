20a057bc7846bbca96f3c43f63777dda0e1fe5fb
Giorgio <tran.giorgio@gmail.com>
2022-11-15 14:52:04 -1000

    Hook up Item collection to shop

--- app/imports/ui/pages/Shop.jsx
+++ app/imports/ui/pages/Shop.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
 import SidebarFull from '../components/sidebar/SidebarFull';
+import { Items } from '../../api/items/Items';
 import Item from '../components/shop/Item';
 
+const Shop = () => {
+  const [showItems, setShowItems] = useState([]);
+  const { items } = useTracker(() => {
+    const allItems = Items.collection.find({}).fetch();
+    return {
+      items: allItems,
+    };
+  }, []);
 
-const Shop = () => (
-  <div className="d-flex">
-    {/* temporarily set the background to dark to test padding */}
-    <SidebarFull />
-    <Container fluid className="min-vh-100">
-      <Row>
+  useEffect(() => {
+    console.log('rendered');
+    setShowItems(items);
+  }, [items.length]);
 
-      </Row>
-    </Container>
-  </div>
-);
+  return (
+    <div className="d-flex">
+      {/* temporarily set the background to dark to test padding */}
+      <SidebarFull />
+      <Container fluid className="min-vh-100">
+        <Row>
+          {showItems.map(item => <Item key={item._id} item={item} />)}
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Shop;
