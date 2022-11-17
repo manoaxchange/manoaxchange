da0c53d48bf29633ca3f29b475b290cf009f30b8
Geean <gtp@hawaii.edu>
2022-11-16 14:31:22 -1000

    Item page template completed

--- app/imports/ui/pages/Item.jsx
+++ app/imports/ui/pages/Item.jsx
@@ -2,32 +2,29 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Button, Container, Row, Col, Image } from 'react-bootstrap';
 
-
 const Item = () => (
-  <Container className="pb-4">
-    <Row className="py-3">
-      <Col>
-        <h2> Item Name </h2>
-      </Col>
-    </Row>
+  <Container className="py-3">
     <Row>
       <Col>
-        <Image src={"http://cdn.akc.org/content/article-body-image/samoyed_puppy_dog_pictures.jpg"} className="imagefix" />
+        <h2 className> ITEM NAME </h2>
+        <Image src="https://images.pexels.com/photos/12211/pexels-photo-12211.jpeg?cs=srgb&dl=pexels-tetyana-kovyrina-12211.jpg&fm=jpg" className="imagefix" />
       </Col>
       <Col>
-        <Row>
-          <Col> <Button variant="success"> Favorite </Button> </Col>
-          <Col> <Button variant="danger"> Report </Button> </Col>
-        </Row>
-        <Row className="py-3">
-          <p> Mitochondria are membrane bound organelles present in almost all eukaryotic cells. Responsible for orchestrating cellular energy production, they are central to the maintenance of life and the gatekeepers of cell death. Thought to have originated from symbiotic ancestors, they carry a residual genome as mtDNA encoding 13 proteins essential for respiratory chain function. Mitochondria comprise an inner and outer membrane that separate and maintain the aqueous regions, the intermembrane space and the matrix. Mitochondria contribute to many processes central to cellular function and dysfunction including calcium signalling, cell growth and differentiation, cell cycle control and cell death. Mitochondrial shape and positioning in cells is crucial and is tightly regulated by processes of fission and fusion, biogenesis and autophagy, ensuring a relatively constant mitochondrial population. Mitochondrial dysfunction is implicated in metabolic and age related disorders, neurodegenerative diseases and ischemic injury in heart and brain.</p>
-        </Row>
-        <Row className="text-center">
-          <h2> 10.99 USD </h2>
-        </Row>
-        <Row>
-          <Button> Message Seller </Button>
-        </Row>
+        <div className="descmargin">
+          <div className="itemdes">
+            <Button className="px-5 buttonspace" variant="success"> Favorite </Button>
+            <Button className="px-5 buttonspace" variant="danger"> Report </Button>
+          </div>
+          <div className="rounded border">
+            <p className="textspace"> The essence of Arendt’s idea was in the fact that the power of many would not become something substantial over the course of time, as the core of Jefferson’s ward system was the power of everyone and the personal competencies of every civilian (254). This turned out to be a...</p>
+          </div>
+          <div className="parent py-3">
+            <div className="child">
+              <h2 className="pb-3"> $100.00 </h2>
+              <Button> Message Seller </Button>
+            </div>
+          </div>
+        </div>
       </Col>
     </Row>
   </Container>
