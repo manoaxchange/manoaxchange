4e567c67a9f9e915f6ad1e466ea4712ac90a65c6
Giorgio <tran.giorgio@gmail.com>
2022-11-18 11:33:16 -1000

    Add fake MyItems and search bar

--- app/imports/ui/pages/MyItems.jsx
+++ app/imports/ui/pages/MyItems.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Button, Container, Form, InputGroup, Row, Col } from 'react-bootstrap';
+import { Search } from 'react-bootstrap-icons';
+import MyItem from '../components/MyItem';
 
-const MyItems = () => (
-  <Container fluid>
-    <Row>
-      <Col xs={4}>
-        <Card>
-          <Card.Header>
-            Test
-          </Card.Header>
-          <Card.Body>
-            Test
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  </Container>
-);
+const randomItems = () => {
+  const arr = [];
+  for (let x = 0; x < 20; x++) {
+    arr.push(<MyItem />);
+  }
+  return arr;
+};
+
+const MyItems = () => {
+  return (
+    <Container className="py-3">
+      <Row>
+        <Col>
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-secondary" id="button-addon2" type="submit">
+              <Search />
+            </Button>
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        {randomItems().map(item => item)}
+      </Row>
+    </Container>
+  );
+};
 
 export default MyItems;
