5f76a1740a354d860ceb228a637488c6fdc1d515
Giorgio <tran.giorgio@gmail.com>
2022-11-18 12:41:37 -1000

    Change buttons style

--- app/imports/ui/components/MyItem.jsx
+++ app/imports/ui/components/MyItem.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { Button, Card, Col, Image } from 'react-bootstrap';
 import { CurrencyDollar, PencilSquare, Trash3Fill } from 'react-bootstrap-icons';
 
+const blackButton = {
+  background: 'none',
+  color: 'black',
+  border: 'none',
+  padding: 0,
+};
+
 const MyItem = () => (
   <Col xs={12} lg={4} xl={3} className="d-flex my-3">
     <Card className="w-100" style={{ minHeight: '300px' }}>
-      <Card.Header className="d-flex justify-content-end gap-1">
-        <Button><PencilSquare /></Button>
-        <Button><CurrencyDollar /></Button>
-        <Button><Trash3Fill /></Button>
+      <Card.Header className="d-flex justify-content-end align-items-center gap-2">
+        <Button style={blackButton}><PencilSquare /></Button>
+        <Button style={blackButton}><CurrencyDollar /></Button>
+        <Button style={blackButton}><Trash3Fill /></Button>
       </Card.Header>
       <Card.Body className="d-flex align-items-center">
         <Image width="100%" height="100%" src="#" style={{ objectFit: 'contain' }} />
