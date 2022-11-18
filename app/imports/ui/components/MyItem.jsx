be920219cb5aeeb9fe365d67ee49c31aa469f2e4
Giorgio <tran.giorgio@gmail.com>
2022-11-18 12:28:50 -1000

    Implement basic buttons on MyItems

--- app/imports/ui/components/MyItem.jsx
+++ app/imports/ui/components/MyItem.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Card, Col, Image } from 'react-bootstrap';
+import { Button, Card, Col, Image } from 'react-bootstrap';
+import { CurrencyDollar, PencilSquare, Trash3Fill } from 'react-bootstrap-icons';
 
 const MyItem = () => (
   <Col xs={12} lg={4} xl={3} className="d-flex my-3">
     <Card className="w-100" style={{ minHeight: '300px' }}>
+      <Card.Header className="d-flex justify-content-end gap-1">
+        <Button><PencilSquare /></Button>
+        <Button><CurrencyDollar /></Button>
+        <Button><Trash3Fill /></Button>
+      </Card.Header>
       <Card.Body className="d-flex align-items-center">
         <Image width="100%" height="100%" src="#" style={{ objectFit: 'contain' }} />
       </Card.Body>
