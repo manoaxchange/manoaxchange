cce968beb9fd24c0b23ab141c448b014b5ed52f5
Giorgio <tran.giorgio@gmail.com>
2022-11-22 09:09:07 -1000

    Add initial files for MyItem

---
+++ app/imports/ui/components/myItem/EditModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditModal = () => (
+  <div> </div>
+);
+
+export default EditModal;
---
+++ app/imports/ui/components/myItem/SoldModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SoldModal = () => (
+  <div> </div>
+);
+
+export default SoldModal;
