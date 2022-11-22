36aa34d5128e7a82e190ab0cce9bbbd8f43fdc5b
Geean <gtp@hawaii.edu>
2022-11-22 09:47:55 -1000

    added item collection to item page

--- app/imports/ui/pages/Item.jsx
+++ app/imports/ui/pages/Item.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
-import { Button, Container, Row, Col, Image } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import Item from '../components/Item';
+import { Items } from '../../api/items/Items';
+import LoadingSpinner from '../components/LoadingSpinner';
 
-const Item = () => (
-  <Container className="py-3">
-    <Row>
-      <Col>
-        <h2 className> ITEM NAME </h2>
-        <Image src="https://images.pexels.com/photos/12211/pexels-photo-12211.jpeg?cs=srgb&dl=pexels-tetyana-kovyrina-12211.jpg&fm=jpg" className="imagefix" />
-      </Col>
-      <Col>
-        <div className="descmargin">
-          <div className="itemdes">
-            <Button className="px-5 buttonspace" variant="success"> Favorite </Button>
-            <Button className="px-5 buttonspace" variant="danger"> Report </Button>
-          </div>
-          <div className="rounded border">
-            <p className="textspace"> The essence of Arendt’s idea was in the fact that the power of many would not become something substantial over the course of time, as the core of Jefferson’s ward system was the power </p>
-          </div>
-          <div className="parent py-3">
-            <div className="child">
-              <h2 className="pb-3"> $100.00 </h2>
-              <Button> Message Seller </Button>
-            </div>
-          </div>
-        </div>
-      </Col>
-    </Row>
-  </Container>
-);
+const Itempages = () => {
 
-export default Item;
+  const { items } = useTracker(() => {
+    const user = Meteor.user();
+    if (user) {
+      const itemItems = Items.collection.find({ owner: user.username }).fetch();
+      console.log(user.username);
+      return {
+        items: itemItems,
+      };
+    }
+    return {
+      items: null,
+    };
+  });
+
+  const itemsReady = () => !!items;
+
+  console.log('items:', items);
+
+  return (itemsReady() ? (
+    <Container className="py-3">
+      {items.map(item => <Item item={item} />)}
+    </Container>
+  ) : <LoadingSpinner />);
+};
+
+export default Itempages;
