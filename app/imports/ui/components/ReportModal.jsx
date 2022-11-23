a41b71f473ba550f5425b5c70770a84b63446e42
Giorgio <tran.giorgio@gmail.com>
2022-11-23 09:02:18 -1000

    Implement report modal to submit report

--- app/imports/ui/components/ReportModal.jsx
+++ app/imports/ui/components/ReportModal.jsx
@@ -2,26 +2,59 @@ import React from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import { Flag } from 'react-bootstrap-icons';
 import PropTypes from 'prop-types';
+import SimpleSchema from 'simpl-schema';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import swal from 'sweetalert';
+import { AutoForm, ErrorsField, LongTextField, SubmitField } from 'uniforms-bootstrap5';
+import { Reports } from '../../api/reports/Reports';
 
-const ReportModal = ({ show, handleClose, item }) => (
-  <Modal show={show} onHide={handleClose}>
-    <Modal.Header closeButton>
-      <Modal.Title>Report <Flag /></Modal.Title>
-    </Modal.Header>
-    <Modal.Body>{`Are you sure you want to delete ${item.name}?`}</Modal.Body>
-    <Modal.Footer>
-      <Button
-        variant="danger"
-        onClick={() => console.log(item._id)}
-      >
-        Submit
-      </Button>
-      <Button variant="secondary" onClick={handleClose}>
-        Cancel
-      </Button>
-    </Modal.Footer>
-  </Modal>
-);
+const formSchema = new SimpleSchema({
+  report: String,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+const ReportModal = ({ show, handleClose, item }) => {
+  const submit = (data, formRef) => {
+    const { report } = data;
+    const itemName = item.name;
+    const itemId = item._id;
+    const owner = item.owner;
+    const createdAt = new Date();
+    Reports.collection.insert(
+      { report, itemName, itemId, owner, createdAt },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item is now in the shop!', 'success');
+          formRef.reset();
+        }
+      },
+    );
+  };
+
+  let fRef = null;
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+        <Modal.Header closeButton>
+          <Modal.Title><Flag /></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <LongTextField name="report" />
+          <ErrorsField />
+        </Modal.Body>
+        <Modal.Footer>
+          <SubmitField value="Submit" />
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </AutoForm>
+    </Modal>
+  );
+};
 
 ReportModal.propTypes = {
   show: PropTypes.bool.isRequired,
@@ -29,6 +62,7 @@ ReportModal.propTypes = {
   item: PropTypes.shape({
     name: PropTypes.string,
     _id: PropTypes.string,
+    owner: PropTypes.string,
   }).isRequired,
 };
 
