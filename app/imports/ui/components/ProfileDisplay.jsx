1e145ef66b26d3b4fb46d4e5db6d5f0e7a8487b8
nickkaw <112596687+nickkaw@users.noreply.github.com>
2022-11-22 17:07:53 -1000

    revamped my-profile page

---
+++ app/imports/ui/components/ProfileDisplay.jsx
@@ -0,0 +1,34 @@
+import { Card, Col, Image } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+/* Component for layout out a Profile Card. */
+const ProfileDisplay = ({ profile }) => (
+  <Col>
+    <Card className="h-100">
+      <Card.Header>
+        <Image src={profile.picture} width={200} />
+      </Card.Header>
+      <Card.Title>{profile.firstName} {profile.lastName}</Card.Title>
+      <Card.Body>
+        <Card.Text>
+          Biography: {profile.bio}
+        </Card.Text>
+        <Card.Text>
+          Overall Rating: [User Rating]
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </Col>
+);
+
+ProfileDisplay.propTypes = {
+  profile: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    bio: PropTypes.string,
+    picture: PropTypes.string,
+  }).isRequired,
+};
+
+export default ProfileDisplay;
