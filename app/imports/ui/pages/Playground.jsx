94ab60754ca105613c099fdbcda39dec688f2089
Giorgio <tran.giorgio@gmail.com>
2022-11-26 22:02:11 -1000

    Add stateful file upload to Playground

--- app/imports/ui/pages/Playground.jsx
+++ app/imports/ui/pages/Playground.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import messageService from '../../services/gmail';
 
 const Playground = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
   const sendMessage = (event) => {
     event.preventDefault();
     const mailOptions = {
@@ -16,7 +17,21 @@ const Playground = () => {
   };
 
   return (
-    <Button onClick={(event) => sendMessage(event)}>Test Me</Button>
+    <div className="d-flex justify-content-center flex-column align-items-center">
+      <Button onClick={(event) => sendMessage(event)}>Send Email</Button>
+      <Form.Group controlId="formFileSm" className="mt-3">
+        <Form.Label>Input</Form.Label>
+        <Form.Control
+          type="file"
+          size="sm"
+          accept="image/jpeg,image/jpg,image/png"
+          onChange={(event) => {
+            setSelectedFile(event.target.files[0]);
+          }}
+        />
+        {console.log(selectedFile)}
+      </Form.Group>
+    </div>
   );
 };
 
