7dbf7afd68b1b448ec35ab83785285cf601a6e89
Giorgio <tran.giorgio@gmail.com>
2022-11-27 19:37:37 -1000

    Implement cloudinary backend and frontend
    Setup cloudinary with env variables. Able to upload images to server. Upon upload, images are retrieved in CloudImg.

--- app/imports/ui/pages/Playground.jsx
+++ app/imports/ui/pages/Playground.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Image } from 'react-bootstrap';
+import axios from 'axios';
 import messageService from '../../services/gmail';
+import CloudImg from '../components/CloudImg';
 
 const Playground = () => {
+
   const [selectedFile, setSelectedFile] = useState(null);
+  const [image, setImage] = useState(null);
+  const [imgId, setImgId] = useState('');
   const sendMessage = (event) => {
     event.preventDefault();
     const mailOptions = {
@@ -14,6 +19,27 @@ const Playground = () => {
     messageService
       .create(mailOptions)
       .then(console.log('message sent, check email'));
+  };
+
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      setImage(reader.result);
+    };
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const result = await axios.post('/api/cloudinary/upload', {
+      image: image,
+    });
+    try {
+      setImgId(result.data.public_id);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -27,10 +53,16 @@ const Playground = () => {
           accept="image/jpeg,image/jpg,image/png"
           onChange={(event) => {
             setSelectedFile(event.target.files[0]);
+            previewFile(event.target.files[0]);
           }}
         />
-        {console.log(selectedFile)}
+        {console.log('selectedFile', selectedFile)}
+        <Button onClick={(event) => handleSubmit(event)}>Upload Image</Button>
       </Form.Group>
+      <h1>Uploaded Image</h1>
+      <Image src={image} />
+      <h1>Image from Cloudinary</h1>
+      <CloudImg uploadedImg={imgId} />
     </div>
   );
 };
---
+++ app/server/cloudinary-index.js
@@ -0,0 +1,33 @@
+const { WebApp } = require('meteor/webapp');
+const express = require('express');
+const cors = require('cors');
+const { Meteor } = require('meteor/meteor');
+const cloudinary = require('./cloudinary/cloudinary');
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+app.get('/cloudinary', async (req, res) => {
+  res.json('hey there');
+});
+
+app.post('/api/cloudinary/upload', async (req, res) => {
+  const { image } = req.body;
+  const uploadedImage = await cloudinary.uploader.upload(image, {
+    upload_preset: Meteor.settings.CLOUDINARY_UPLOADPRESET,
+    allowed_formats: ['png', 'jpg', 'jpeg', 'svg', 'ico', 'webp'],
+  }, (error, result) => {
+    if (error) console.log(error);
+    console.log(result);
+  });
+
+  try {
+    res.status(200).json(uploadedImage);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+WebApp.connectHandlers.use(app);
