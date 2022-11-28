b13d8e46077d7a5197921e69d3388e786a5b10c8
Giorgio <tran.giorgio@gmail.com>
2022-11-27 20:11:29 -1000

    Change gmail service to async await

--- app/imports/services/gmail.js
+++ app/imports/services/gmail.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const baseUrl = '/api/mail/send';
 
-const create = newMsgObject => {
-  const request = axios.post(baseUrl, newMsgObject);
-  return request.then(response => response.data);
+const create = async (newMsgObject) => {
+  const result = await axios.post(baseUrl, newMsgObject);
+  return result;
 };
 
 export default { create };
