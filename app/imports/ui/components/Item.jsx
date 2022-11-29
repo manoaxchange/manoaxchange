4520ae664c9ddf01a2a704ac043a5275dbb612a8
Giorgio <tran.giorgio@gmail.com>
2022-11-28 22:17:08 -1000

    Implement Message Seller button and Report Modal

--- app/imports/ui/components/Item.jsx
+++ app/imports/ui/components/Item.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import MessageModal from './MessageModal';
+import ReportModal from './ReportModal';
 
 const Item = ({ item }) => {
+  const currentUser = useTracker(() => Meteor.user(), []);
   const [show, setShow] = useState(false);
   const handleShow = () => { setShow(true); };
   const handleClose = () => { setShow(false); };
+
+  const [showReport, setShowReport] = useState(false);
+  const handleShowReport = () => { setShowReport(true); };
+  const handleCloseReport = () => { setShowReport(false); };
 
   return (
     <Container className="py-3">
@@ -19,7 +27,7 @@ const Item = ({ item }) => {
           <div className="descmargin">
             <div className="itemdes">
               <Button className="px-5 buttonspace" variant="success"> Favorite </Button>
-              <Button className="px-5 buttonspace" variant="danger"> Report </Button>
+              <Button className="px-5 buttonspace" variant="danger" onClick={handleShowReport}> Report </Button>
             </div>
             <div className="rounded border">
               <p className="textspace"> {`${item.description}`} </p>
@@ -27,13 +35,14 @@ const Item = ({ item }) => {
             <div className="parent py-3">
               <div className="child">
                 <h2 className="child pb-3"> ${`${item.price}`} </h2>
-                <Button onClick={handleShow}> Message Seller </Button>
+                {currentUser ? <Button onClick={handleShow}> Message Seller </Button> : ''}
               </div>
             </div>
           </div>
         </Col>
       </Row>
-      <MessageModal handleClose={handleClose} show={show} item={item}/>
+      <MessageModal handleClose={handleClose} show={show} item={item} />
+      <ReportModal item={item} handleClose={handleCloseReport} show={showReport} />
     </Container>
   );
 };
--- app/imports/ui/components/MessageModal.jsx
+++ app/imports/ui/components/MessageModal.jsx
@@ -1,35 +1,63 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Button, Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const MessageModal = ({ show, handleClose, item }) => (
-  <Modal show={show} onHide={handleClose}>
-    <Modal.Header closeButton>
-      <Modal.Title>Message Seller</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      You are alerting the seller that you are interested in this item.  Both you and the seller will receive an email from ManoaXchange. From there on, please communicate via email. Click <b>Send Message</b> to continue.
-    </Modal.Body>
-    <Modal.Footer>
-      <Button
-        variant="danger"
-        onClick={() => {
-          console.log('sent');
-          handleClose();
-        }}
-      >
-        Message
-      </Button>
-      <Button variant="secondary" onClick={handleClose}>
-        Cancel
-      </Button>
-    </Modal.Footer>
-  </Modal>
-);
+const MessageModal = ({ show, handleClose, item }) => {
+  const currentUser = useTracker(() => Meteor.user().username, []);
+  const seller = item.owner;
+  const sendMessage = async (event) => {
+    event.preventDefault();
+    const mailOptions = {
+      to: [seller, currentUser],
+      subject: `[MANOAXCHANGE] ${item.name}`,
+      text: `BUYER: ${currentUser}, SELLER: ${seller}`,
+    };
+
+    try {
+      const result = await axios.post('/api/mail/send', mailOptions);
+      console.log(result.status);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return currentUser ? (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Message Seller</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        You are alerting the seller that you are interested in this item. Both you and the seller will receive an email from ManoaXchange. From there on, please communicate via email. Click <b>Send Message</b> to continue.
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="danger"
+          onClick={(event) => {
+            sendMessage(event);
+            handleClose();
+          }}
+        >
+          Message
+        </Button>
+
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  ) : '';
+};
 
 MessageModal.propTypes = {
   show: PropTypes.bool.isRequired,
   handleClose: PropTypes.func.isRequired,
+  item: PropTypes.shape({
+    owner: PropTypes.string,
+    name: PropTypes.string,
+  }).isRequired,
 };
 
 export default MessageModal;
