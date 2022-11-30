c7a8a0567c9a9195bce0f1bf55f311d7954782c5
nickkaw <112596687+nickkaw@users.noreply.github.com>
2022-11-29 14:54:57 -1000

    added sell item form test

--- app/tests/sell.page.js
+++ app/tests/sell.page.js
@@ -1,4 +1,5 @@
 import { Selector } from 'testcafe';
+import { navBar } from './navbar.component';
 import { PAGE_IDS } from '../imports/ui/utilities/PageIDs';
 import { COMPONENT_IDS } from '../imports/ui/utilities/ComponentIDs';
 
@@ -11,7 +12,7 @@ class SellPage {
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
     // This is first test to be run. Wait 10 seconds to avoid timeouts with GitHub Actions.
-    await testController.wait(10000).expect(this.pageSelector.exists).ok();
+    await testController.wait(3000).expect(this.pageSelector.exists).ok();
   }
 
   async sellItem(testController, credentials) {
@@ -20,9 +21,16 @@ class SellPage {
     await testController.typeText(`#${COMPONENT_IDS.SELL_FORM_PRICE}`, credentials.price);
     await testController.typeText(`#${COMPONENT_IDS.SELL_FORM_DESCRIPTION}`, credentials.description);
     await testController.typeText(`#${COMPONENT_IDS.SELL_FORM_IMAGE}`, credentials.image);
-    const categorySelector = Selector(`#${COMPONENT_IDS.SELL_FORM_CATEGORY} div.form-select`);
-    await testController.click(categorySelector.nth(1));
-    await testController.click(`#${COMPONENT_IDS.SELL_FORM_SUBMIT} input.btn.btn-primary`)
+    const categorySelector = Selector(`#${COMPONENT_IDS.SELL_FORM_CATEGORY}`);
+    await testController.click(categorySelector);
+    const categoryOption = categorySelector.find('option');
+    await testController.click(categoryOption.withText('Miscellaneous'));
+    await testController.expect(categorySelector.value).eql('Miscellaneous');
+    await testController.click(`#${COMPONENT_IDS.SELL_FORM_SUBMIT} input.btn.btn-primary`);
+    await testController.click('button.swal-button--confirm');
+    await navBar.gotoMyItemsPage(testController);
+    const cardCount = Selector('.card').count;
+    await testController.expect(cardCount).gte(2);
   }
 }
 
