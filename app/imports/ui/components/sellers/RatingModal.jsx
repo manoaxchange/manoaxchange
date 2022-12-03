b12d1586506ac5eb21d8a55ee24a9b5020d74b14
nickkaw <112596687+nickkaw@users.noreply.github.com>
2022-12-03 11:14:24 -1000

    ratings modal checkpoint

--- app/imports/ui/components/sellers/RatingModal.jsx
+++ app/imports/ui/components/sellers/RatingModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import { AutoForm, ErrorsField, LongTextField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, RadioField, SubmitField } from 'uniforms-bootstrap5';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import swal from 'sweetalert';
 import { Profiles } from '../../../api/profiles/Profiles';
@@ -12,24 +12,24 @@ const bridge = new SimpleSchema2Bridge(Ratings.schema);
 
 const RatingModal = ({ show, handleClose, profile, rating }) => {
   const submit = (data) => {
-    const { profileId, userId, value } = data;
-    const ratedBefore = Ratings.collection.find({ userId: rating.userId, profileId: rating.profileId }).count();
+    const { profileName, userName, value } = data;
+    const ratedBefore = Ratings.collection.find({ userName: rating.userName, profileName: rating.profileName }).count();
     if (ratedBefore === 1) {
       handleClose();
-      Ratings.collection.update(rating._id, { $set: { profileId, userId, value } }, (error) => (
+      Ratings.collection.update(rating._id, { $set: { profileName, userName, value } }, (error) => (
         error
           ? swal('Error', error.message, 'error')
           : swal('Success', 'ItemDetails updated successfully', 'success')
       ));
     } else {
       handleClose();
-      Ratings.collection.add({ profileId, userId, value }, (error) => (
+      Ratings.collection.add({ profileName, userName, value }, (error) => (
         error
           ? swal('Error', error.message, 'error')
           : swal('Success', 'ItemDetails updated successfully', 'success')
       ));
     }
-    Profiles.collection.update(profile._id, { $set: { profileId, userId, value } }, (error) => (
+    Profiles.collection.update(profile._id, { $set: { profileName, userName, value } }, (error) => (
       error
         ? swal('Error', error.message, 'error')
         : swal('Success', 'ItemDetails updated successfully', 'success')
@@ -38,20 +38,16 @@ const RatingModal = ({ show, handleClose, profile, rating }) => {
 
   return (
     <Modal show={show} onHide={handleClose}>
-      <AutoForm schema={bridge} onSubmit={data => submit(data)} model={item}>
+      <AutoForm schema={bridge} onSubmit={data => submit(data)}>
         <Modal.Header closeButton>
           <Modal.Title>Edit</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <TextField id={COMPONENT_IDS.EDIT_ITEM_FORM_NAME} name="name" />
-          <NumField name="price" decimal />
-          <LongTextField name="description" />
-          <TextField name="image" />
-          <SelectField name="category" />
+          <RadioField name="rating" inline showInlineError labelClassName="px-5" />
           <ErrorsField />
+          <SubmitField id={COMPONENT_IDS.EDIT_ITEM_FORM_SUBMIT} value="Save" />
         </Modal.Body>
         <Modal.Footer>
-          <SubmitField id={COMPONENT_IDS.EDIT_ITEM_FORM_SUBMIT} value="Save" />
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
@@ -72,8 +68,8 @@ RatingModal.propTypes = {
     _id: PropTypes.string,
   }).isRequired,
   rating: PropTypes.shape({
-    profileId: PropTypes.string,
-    userId: PropTypes.string,
+    profileName: PropTypes.string,
+    userName: PropTypes.string,
     value: PropTypes.number,
     _id: PropTypes.string,
   }).isRequired,
