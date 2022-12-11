7a0685bb2864cd377d3a2d595fa9366d09513052
Giorgio <tran.giorgio@gmail.com>
2022-12-11 00:42:47 -1000

    Rearrange ProfileDisplay and SellerProfile

--- app/imports/ui/components/profile/ProfileDisplay.jsx
+++ app/imports/ui/components/profile/ProfileDisplay.jsx
@@ -1,44 +1,53 @@
-import { Card, Col, Container, Image } from 'react-bootstrap';
-import { _ } from 'meteor/underscore';
+import { Container, Image } from 'react-bootstrap';
+import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { Ratings } from '../../../api/ratings/Ratings';
+import RatingModal from '../sellers/RatingModal';
 
 /* Component for layout out a Profile Card. */
 
 const ProfileDisplay = ({ profile }) => {
-  const count = Ratings.collection.find({ profileId: profile._id }).count() - 1;
-  let total = Ratings.collection.find({ profileId: profile._id }).fetch();
-  total = _.pluck(total, 'value');
-  total = _.reduce(total, function (memo, num) { return memo + num; }, 0);
-  console.log(count, total);
+  const [show, setShow] = useState(false);
+  const handleShow = () => { setShow(true); };
+  const handleClose = () => { setShow(false); };
+
+  const ratings = Ratings.collection.find({ profileId: profile._id }).fetch();
+  const hasRatedBefore = ratings.filter(rating => rating.userEmail === Meteor.user().username);
+  const averageRating = (arr) => {
+    if (arr.length < 2) {
+      return '';
+    }
+    const temp = arr.slice(0, -1);
+    if (temp.length === 1) {
+      return temp[0].value;
+    }
+    return temp.reduce((prev, curr) => prev.value + curr.value) / temp.length;
+  };
+
   return (
-    <>
-      {/* <Card className="h-100"> */}
-      {/*  <Card.Header> */}
-      {/*    <Image src={profile.picture} width={200} /> */}
-      {/*  </Card.Header> */}
-      {/*  <Card.Title>{profile.firstName} {profile.lastName}</Card.Title> */}
-      {/*  <Card.Body> */}
-      {/*    <Card.Text> */}
-      {/*      Biography: {profile.bio} */}
-      {/*    </Card.Text> */}
-      {/*    <Card.Text> */}
-      {/*      Overall Rating: {total / count} / 5 */}
-      {/*    </Card.Text> */}
-      {/*  </Card.Body> */}
-      {/* </Card> */}
+    <div className="py-3" style={{ backgroundColor: '#ECECEC' }}>
       <Container fluid className="d-flex justify-content-center">
-        <div className="h-25">
+        <div className="h-25 d-flex flex-column justify-content-center gap-2">
           <div style={{ height: '300px', width: '300px' }}>
             <Image src={profile.picture} height="100%" width="100%" style={{ objectFit: 'cover', borderRadius: '3px' }} />
           </div>
           <div>
-            {`${profile.firstName.toUpperCase()} ${profile.lastName.toUpperCase()}`}
+            <div style={{ fontWeight: 'normal' }} className="h5 d-flex justify-content-between">
+              <div>{`${profile.firstName.toUpperCase()} ${profile.lastName.toUpperCase()}`}</div>
+              <div>
+                <span>{averageRating(ratings) ? averageRating(ratings).toFixed(2) : 'Not Rated Yet'}</span>
+                <span>&nbsp;({ratings.length - 1})</span>
+              </div>
+            </div>
+            <button type="button" onClick={handleShow}>
+              Rate This User
+            </button>
           </div>
         </div>
       </Container>
-    </>
+      <RatingModal handleClose={handleClose} profile={profile} show={show} rating={hasRatedBefore} />
+    </div>
   );
 };
 
--- app/imports/ui/pages/SellerProfile.jsx
+++ app/imports/ui/pages/SellerProfile.jsx
@@ -8,15 +8,12 @@ import { Profiles } from '../../api/profiles/Profiles';
 import ProfileDisplay from '../components/profile/ProfileDisplay';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { Items } from '../../api/items/Items';
-import Selling from '../components/sellings/SellerItemCard';
 import SellersItemDisplay from '../components/sellers/SellersItemDisplay';
 import { Ratings } from '../../api/ratings/Ratings';
 import RatingModal from '../components/sellers/RatingModal';
+import ItemCard2 from '../components/shop/ItemCard2';
 
 const SellerProfile = () => {
-  const [show, setShow] = useState(false);
-  const handleShow = () => { setShow(true); };
-  const handleClose = () => { setShow(false); };
   const { _id } = useParams();
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, profiles, items, ratings } = useTracker(() => {
@@ -30,14 +27,9 @@ const SellerProfile = () => {
     const rdy = subscription1.ready() && subscription2.ready() && subscription3.ready();
     // Get the Stuff documents
     const profileItems = Profiles.collection.find({ _id: _id }).fetch();
-    console.log('profile items', profileItems);
     const itemOwner = () => (profileItems.length !== 0 ? profileItems[0].owner : '');
     const sellerItems = Items.collection.find({ owner: itemOwner() }).fetch();
     const ratingDocs = Ratings.collection.find({ profileId: _id }).fetch();
-    console.log(ratingDocs);
-    console.log(profileItems, profileItems);
-    console.log(sellerItems, sellerItems);
-    console.log('_id', _id);
     return {
       profiles: profileItems,
       items: sellerItems,
@@ -47,29 +39,12 @@ const SellerProfile = () => {
   }, [_id]);
 
   return (ready ? (
-    <Container id={PAGE_IDS.USER_PROFILE} className="py-3">
-      <Row className="justify-content-center">
-        <Col md={7}>
-          <Col className="text-center">
-            {profiles.map((profile) => <ProfileDisplay key={`profile-${profile._id}`} profile={profile} />)}
-          </Col>
-        </Col>
+    <div id={PAGE_IDS.USER_PROFILE}>
+      <ProfileDisplay key={`profile-${profiles[0]._id}`} profile={profiles[0]} />
+      <Row className="w-100">
+        {items.map((item) => <ItemCard2 key={`item-${item._id}`} item={item} />)}
       </Row>
-      <Container className="d-flex justify-content-evenly py-3">
-        <Button className="d-inline-block" onClick={handleShow}>Rate Profile</Button>
-      </Container>
-      <Container>
-        <Row className="text-center">
-          {profiles.map((profile) => <SellersItemDisplay key={`profile-${profile._id}`} profile={profile} />)}
-        </Row>
-      </Container>
-      <Container>
-        <Row className="justify-content-center">
-          {items.map((item) => <Selling key={`item-${item._id}`} item={item} />)}
-        </Row>
-      </Container>
-      {profiles.map((profile) => <RatingModal key={profile._id} handleClose={handleClose} show={show} rating={ratings.filter((rating) => rating.userEmail === 'empty' || rating.userEmail === Meteor.user().username)} profile={profile} />)}
-    </Container>
+    </div>
   ) : <LoadingSpinner />);
 };
 
